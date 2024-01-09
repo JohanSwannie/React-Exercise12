c39b6a512df4bdf1d86f316dd4398074e0b28baa
Johan Swan <jswannieman@gmail.com>
2024-01-10 10:51:51 +1300

    Update Card JSX Component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Card() {
   return (
     <div>
-      <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+      <figure className="bg-slate-400 rounded-xl p-12">
         <img
           className="w-55 h-40 rounded-full mx-auto"
           src="https://picperf.io/https://laravelnews.s3.amazonaws.com/images/tailwindcss-1633184775.jpg"
@@ -15,11 +15,9 @@ function Card() {
               Tailwind CSS is an excellent framework.
             </p>
           </blockquote>
-          <figcaption className="font-medium">
-            <div className="text-sky-500 dark:text-sky-400 mb-8">
-              Johan Swan
-            </div>
-            <div className="text-slate-700 dark:text-slate-500">
+          <figcaption className="font-bold">
+            <div className="text-blue-600 mb-8">Johan Swan</div>
+            <div className="text-slate-950">
               Front End Developer New Zealand
             </div>
           </figcaption>
