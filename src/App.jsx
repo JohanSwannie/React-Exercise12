db7ec531659e057f5fb46faded9944ff5151ea80
Johan Swan <jswannieman@gmail.com>
2024-04-01 10:36:29 +1300

    Update App JSX Component

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <h1 className="text-3xl text-center bg-green-500 p-3 mb-5">
         React with Tailwind CSS
       </h1>
-      <div style={{ backgroundColor: backgroundColor, height: "100vh" }}>
+      <div style={{ backgroundColor: `${backgroundColor}`, height: "100vh" }}>
         <div className="grid grid-cols-2 mx-auto w-fit gap-8">
           <button
             className="bg-blue-800 text-white p-3"
